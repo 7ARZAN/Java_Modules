7e80564bffbaf39783ab7cb246af7e6e0b024eef
elhousseine lakhfif <akaouam1@gmail.com>
2025-06-21 16:09:05 +0100

    ex01

--- Java_Modules/Java_Module_00/ex01/Program.java
+++ Java_Modules/Java_Module_00/ex01/Program.java
@@ -1,16 +1,78 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Program
+{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = getValidInput(scanner);
+        scanner.close();
+        
+        PrimeResult result = isPrime(inputNumber);
+        System.out.printf("%b %d%n", result.isPrime, result.iterations);
+    }
+    
+    private static class PrimeResult{
+        boolean isPrime;
+        int iterations;
+        
+        PrimeResult(boolean isPrime, int iterations){
+            this.isPrime = isPrime;
+            this.iterations = iterations;
+        }
+    }
+    
+    private static int getValidInput(Scanner scanner){
+        while (true){
+            try {
+                System.out.println("Enter a number: ");
+                int number = scanner.nextInt();
+                if (number < 2){
+                    System.err.println("IllegalArgument");
+                    continue;
+                }
+                return number;
+            } catch (InputMismatchException e){
+                System.err.println("Error: Please enter a valid integer.");
+                scanner.nextLine();
+            }
+        }
+    }
+    
+    private static PrimeResult isPrime(int number){
+        if (number == 2){
+            return new PrimeResult(true, 1);
+        }
+        
+        if (number % 2 == 0){
+            return new PrimeResult(false, 1);
+        }
+        
+        int iterations = 1;
+        for (int divisor = 3; divisor * divisor <= number; divisor += 2){
+            iterations++;
+            if (number % divisor == 0){
+                return new PrimeResult(false, iterations);
+            }
+        }
+        return new PrimeResult(true, iterations);
+    }
+}
+/*
+
 import java.util.*;
 
-public class Program {
+public class Program
+{
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         boolean validInput = false;
-        int index = 0;
+        int number = 0;
         
         while (!validInput){
             try {
-                index = scanner.nextInt();
-                
-                if (index < 2){
+                number = scanner.nextInt();
+                if (number < 2){
                     System.err.println("IllegalArgument");
                     continue;
                 }
@@ -21,17 +83,16 @@ public class Program {
             }
         }
         boolean isPrime = true;
-        int steps = 1;        
-        for (int i = 2; i * i <= index; i++){
-            if (index % i == 0){
+        int steps = 1;
+        for (int i = 2; i * i <= number; i++){
+            if (number % i == 0){
                 isPrime = false;
                 break;
             }
             steps++;
         }
-        if (index > 2 && steps == 0)
-            steps = 1;
         System.out.println(isPrime + " " + steps);
         scanner.close();
     }
 }
+*/
